4a82df49cc726e4e9d40c158d87c656c522327b6
Ricardo Bueno Balbis <ricardo.bueno@centrohub.co>
2023-12-18 21:06:51 -0500

    config remote database

--- src/main/kotlin/com/ciudadania/CiudadaniaApplication.kt
+++ src/main/kotlin/com/ciudadania/CiudadaniaApplication.kt
@@ -1,12 +1,13 @@
 package com.ciudadania
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
 class CiudadaniaApplication
 
 fun main(args: Array<String>) {
-    runApplication<CiudadaniaApplication>(*args)
+    //runApplication<CiudadaniaApplication>(*args)
+    SpringApplication.run(CiudadaniaApplication::class.java, *args)
 }
